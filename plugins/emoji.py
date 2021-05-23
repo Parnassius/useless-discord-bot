f365d8ab4035525bdaa9782377a924766c3c2877
Parnassius <Parnassius@users.noreply.github.com>
2021-05-23 20:51:35 +0200

    Improve .coloremoji

--- plugins/emoji.py
+++ plugins/emoji.py
@@ -1,14 +1,17 @@
+import io
 import xml.etree.ElementTree as ET
 
 import aiohttp
+import discord  # type: ignore
 from cairosvg import svg2png  # type: ignore
 from discord.ext import commands  # type: ignore
+from PIL import Image, ImageColor, ImageDraw  # type: ignore
 
 from bot import MyBot
 
-TWEMOJI_URL = (
-    "https://raw.githubusercontent.com/twitter/twemoji/master/assets/svg/{}.svg"
-)
+TWEMOJI_URL = "https://raw.githubusercontent.com/twitter/twemoji/master/assets/"
+TWEMOJI_SVG_URL = f"{TWEMOJI_URL}svg/{{}}.svg"
+TWEMOJI_PNG_URL = f"{TWEMOJI_URL}72x72/{{}}.png"
 
 
 def to_code_point(unicode_surrogates: str) -> str:
@@ -37,22 +40,64 @@ def setup(bot: MyBot) -> None:
     ) -> None:
         ET.register_namespace("", "http://www.w3.org/2000/svg")
 
-        emoji_url = TWEMOJI_URL.format(to_code_point(emoji))
+        emoji_code_point = to_code_point(emoji)
+        emoji_svg_url = TWEMOJI_SVG_URL.format(emoji_code_point)
+        emoji_png_url = TWEMOJI_PNG_URL.format(emoji_code_point)
         async with aiohttp.ClientSession() as session:
-            async with session.get(emoji_url) as resp:
+            async with session.get(emoji_svg_url) as resp:
                 if resp.status != 200:
                     await ctx.reply("Emoji not found.")
                     return
                 svg = ET.fromstring(await resp.text())
 
         tags = svg.findall(".//*[@fill]")
-        if len(tags) != len(colors):
+        original_colors = sorted(
+            {i.get("fill") for i in tags}  # type: ignore[type-var]
+        )
+
+        if len(original_colors) != len(colors):
+            try:
+                ImageColor.getrgb(emoji_name)
+            except ValueError:
+                pass
+            else:
+                colors = (emoji_name, *colors)
+                emoji_name = "new_emoji"
+
+        if len(original_colors) != len(colors):
+            with Image.new("RGB", (420, 100)) as im:
+                draw = ImageDraw.Draw(im)
+                section_width = im.width / len(original_colors)
+                for n, c in enumerate(original_colors):
+                    start = int(section_width * n)
+                    end = int(section_width * (n + 1))
+                    draw.rectangle(
+                        [(start, 0), (end, im.height)],
+                        ImageColor.getrgb(c),
+                    )
+                colors_image = io.BytesIO()
+                im.save(colors_image, format="PNG")
+            colors_image.seek(0)
+
+            embed = discord.Embed(title="Original colors")
+
+            embed.set_thumbnail(url=emoji_png_url)
+            embed.set_image(url="attachment://colors.png")
+            for n, c in enumerate(original_colors, 1):
+                embed.add_field(name=f"Color {n}", value=c)
             await ctx.reply(
-                f"This emoji needs {len(tags)} colors, {len(colors)} were provided."
+                (
+                    f"This emoji needs {len(original_colors)} colors, "
+                    f"{len(colors)} were provided."
+                ),
+                file=discord.File(colors_image, filename="colors.png"),
+                embed=embed,
             )
             return
-        for tag, color in zip(tags, colors):
-            tag.set("fill", color)
+
+        color_mappings = dict(zip(original_colors, colors))
+        for tag in tags:
+            tag.set("fill", color_mappings[tag.get("fill")])
 
         img = svg2png(bytestring=ET.tostring(svg, encoding="unicode"), output_width=128)
 
