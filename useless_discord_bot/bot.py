a11eaa391b3249d02ac5bba9fb8f25dea083ab8c
Parnassius <Parnassius@users.noreply.github.com>
2022-02-15 15:16:56 +0100

    Update error handlers for application commands

--- useless_discord_bot/bot.py
+++ useless_discord_bot/bot.py
@@ -1,13 +1,13 @@
 # pylint: disable=no-self-use
 
 from sys import exc_info
-from traceback import format_tb
+from traceback import format_exception
 from typing import Any
 
-from disnake.abc import GuildChannel, Messageable
+from disnake.abc import GuildChannel
 from disnake.channel import TextChannel
-from disnake.errors import HTTPException
 from disnake.ext import commands
+from disnake.interactions.application_command import ApplicationCommandInteraction
 
 
 class MyBot(commands.Bot):
@@ -29,30 +29,39 @@ class MyBot(commands.Bot):
 
     async def on_error(self, event_method: str, *args: Any, **kwargs: Any) -> None:
         # pylint: disable=unused-argument
-        target: Messageable = await self.fetch_user(self.owner_id)
-        for i in args:
-            if isinstance(i, Messageable):
-                target = i
-                break
-        _, val, tb = exc_info()
+        target = await self.fetch_user(self.owner_id)
+        tb = format_exception(*exc_info())
         await target.send(
-            f"Handler `{event_method}` raised an exception: {val}\n"
-            f"```{''.join(format_tb(tb))}```"
+            f"Handler `{event_method}` raised an exception:\n" f"```{''.join(tb)}```"
         )
 
-    async def on_command_error(
-        self, context: commands.Context, exception: commands.CommandError
+    async def on_message_command_error(
+        self,
+        interaction: ApplicationCommandInteraction,
+        exception: commands.CommandError,
     ) -> None:
-        if isinstance(exception, commands.errors.CommandNotFound):
-            return
-        msg = str(exception)
-        if (
-            isinstance(exception, commands.errors.CommandInvokeError)
-            and not isinstance(exception.original, HTTPException)
-            and exception.__traceback__
-        ):
-            msg += f"\n```{''.join(format_tb(exception.__traceback__))}```"
-        await context.reply(msg)
+        target = await self.fetch_user(self.owner_id)
+        command = interaction.application_command.name
+        tb = format_exception(type(exception), exception, exception.__traceback__)
+        await target.send(
+            f"Message command `{command}` raised an exception:\n" f"```{''.join(tb)}```"
+        )
+
+        await interaction.send(str(exception), ephemeral=True)
+
+    async def on_slash_command_error(
+        self,
+        interaction: ApplicationCommandInteraction,
+        exception: commands.CommandError,
+    ) -> None:
+        target = await self.fetch_user(self.owner_id)
+        command = interaction.application_command.name
+        tb = format_exception(type(exception), exception, exception.__traceback__)
+        await target.send(
+            f"Slash command `{command}` raised an exception:\n" f"```{''.join(tb)}```"
+        )
+
+        await interaction.send(str(exception), ephemeral=True)
 
     async def on_guild_channel_create(self, channel: GuildChannel) -> None:
         if not isinstance(channel, TextChannel):
