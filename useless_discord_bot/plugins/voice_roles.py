e7b9c2f9a3841cc4aeb7e837317a1490fb0ed0b7
Parnassius <Parnassius@users.noreply.github.com>
2022-11-25 18:24:10 +0100

    Add voice channels roles

---
+++ useless_discord_bot/plugins/voice_roles.py
@@ -0,0 +1,28 @@
+from discord import Member, VoiceState
+
+from useless_discord_bot.bot import MyBot
+
+
+async def setup(bot: MyBot) -> None:
+    @bot.listen()
+    async def on_voice_state_update(
+        member: Member, before: VoiceState, after: VoiceState
+    ) -> None:
+        if not member.guild.me.guild_permissions.manage_roles:
+            return
+
+        if before.channel is not None:
+            role = next(
+                (x for x in member.guild.roles if x.name == f"call-{before.channel}"),
+                None,
+            )
+            if role:
+                await member.remove_roles(role)
+
+        if after.channel is not None:
+            role = next(
+                (x for x in member.guild.roles if x.name == f"call-{after.channel}"),
+                None,
+            )
+            if role:
+                await member.add_roles(role)
